a84faa5b388b96a824e7334955b8b8ccb4f0d3a5
swalihkzr1111 <mswalih0999@gmail.com>
2025-03-03 13:48:31 +0530

    second_commit

---
+++ lib/03-03-2025/signup.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Signup extends StatelessWidget {
+  const Signup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 80,
+            width: 80,
+          )
+        ],
+      ),
+    );
+  }
+}
